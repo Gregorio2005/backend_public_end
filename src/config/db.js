4da0ec5cbf7b6924b26d2f371b948c8389015213
Gregorio2005 <gsdm07@gmail.com>
2026-05-07 09:00:01 -0400

    cambios_readme

--- src/config/db.js
+++ src/config/db.js
@@ -6,20 +6,20 @@ require('dotenv').config();
 // ============================================================================
 
 // --- OPCIÓN 1: SERVIDOR LOCAL ---
-
+/*
 const dbConfig = {
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME || 'sealing_products_sa',
     password: process.env.DB_PASSWORD || 'Oficial1.com',
     port: process.env.DB_PORT || 5432,
-};
+};*/
 
 // --- OPCIÓN 2: SERVIDOR EN LA NUBE (NEON) ---
-/*
+
 const dbConfig = {
     connectionString: 'postgresql://neondb_owner:npg_otaHCG8I2WPT@ep-calm-voice-ap4zb4d3-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require',
-};*/
+};
 
 const pool = new Pool(dbConfig);
 
